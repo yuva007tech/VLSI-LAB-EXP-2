19e917dca26b5a3802122abc225e0f840493abe6
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 14:04:00 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,12 @@
-module decoder_8(a,b,c,y);
-input a,b,c;
-output[7:0]y;
-
-
-
+module decoder(
+    input [2:0] a,
+    output [7:0] d );
+assign d[0]=(~a[2])&(~a[1])&(~a[0]);
+assign d[1]=(~a[2])&(~a[1])&(a[0]);
+assign d[2]=(~a[2])&(a[1])&(~a[0]);
+assign d[3]=(~a[2])&(a[1])&(a[0]);
+assign d[4]=(a[2])&(~a[1])&(~a[0]);
+assign d[5]=(a[2])&(~a[1])&(a[0]);
+assign d[6]=(a[2])&(a[1])&(~a[0]);
+assign d[7]=(a[2])&(a[1])&(a[0]);
 endmodule
