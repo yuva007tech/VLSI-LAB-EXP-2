026badc42bb4223c28a6c32b8347b1743689cf0a
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 14:05:39 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,13 @@
-module demux_8(s,d,y);
-input [2:0]s;
-input d;
-output [7:0]y;
-
-
-
-
+module demux(din,s,d);
+input din;
+input[2:0]s;
+output [7:0]d;
+assign d[0]=(din&~s [2]&~s[1]&~s[0]);
+assign d[1]=(din&~s[2]&~s[1]&s[0]);
+assign d[2]=(din&~s[2]&s[1]&~s[0]);
+assign d[3]=(din&~s[2]&s[1]&s[0]);
+assign d[4]=(din&s[2]&~s[1]&~s[0]);
+assign d[5]=(din&s[2]&~s[1]&s[0]);
+assign d[6]=(din&s[2]&s[1]&~s[0]);
+assign d[7]=(din&s[2]&s[1]&s[0]);
 endmodule
