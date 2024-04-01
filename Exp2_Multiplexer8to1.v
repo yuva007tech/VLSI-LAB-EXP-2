c3f170622f7fe20040af1a91d95753d0c11c3bec
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 14:09:01 +0530

    Update Exp2_Multiplexer8to1.v

--- Exp2_Multiplexer8to1.v
+++ Exp2_Multiplexer8to1.v
@@ -1,8 +1,19 @@
-module mux_8(s0,s1,s2,d,y);
-input s0,s1,s2;
-input [7:0]d;
+module mux(a,s,y);
+input [7:0]a;
+input [2:0]s;
 output y;
-
-
-
+reg y;
+always@({s ,a})
+   begin
+      case(s)
+         3'b000: y=a[0];
+         3'b001: y=a[1];
+         3'b010: y=a[2];
+         3'b011: y=a[3];
+         3'b100: y=a[4];
+         3'b101: y=a[5];
+         3'b110: y=a[6];
+         3'b111: y=a[7];
+      endcase
+   end
 endmodule
