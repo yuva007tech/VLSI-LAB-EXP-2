ae45b0669dcb01385138bb35e002b6bfae34755b
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 14:07:30 +0530

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,7 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
-output a,b,c;
-
-
-
+module encoder_8_to_3(a0,a1,a2,d7,d6,d5,d4,d3,d2,d1,d0);
+input d7,d6,d5,d4,d3,d2,d1,d0;
+output a0,a1,a2;
+or g1(a0,d1,d3,d5,d7);
+or g2(a1,d2,d3,d6,d7);
+or g3(a2,d4,d5,d6,d7);
 endmodule
